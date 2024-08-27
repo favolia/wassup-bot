77b2ca92c00c26604ee60a94c632aac8433def19
thoriqazzikraa <thoriqazzikra17@gmail.com>
2024-08-27 09:27:20 +0700

    .

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 /** @type {{client: import("@mengkodingan/ckptw").IClientOptions}} */
 module.exports.config = {
   opts: {
-    owner: ["628xxx@s.whatsapp.net", "628xxx@s.whatsapp.net"]
+    owner: ["62895334951166@s.whatsapp.net", "628875090455@s.whatsapp.net"]
   },
   client: {
     prefix: ["/", "!", "#", ''],
@@ -9,7 +9,7 @@ module.exports.config = {
     authDir: "session",
     printQRInTerminal: false, // set ke false jika menggunakan pairing mode
     usePairingCode: true, // pairing mode
-    phoneNumber: "628xxx", // nomor target pairing
+    phoneNumber: "62895334951166", // nomor target pairing
     readIncommingMsg: false,
     WAVersion: [2, 3000, 1015901307], // add this
   },
